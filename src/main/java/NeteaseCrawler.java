11cccdcfc00e080de317bf99c1fb2d53ecb999f3
voidif <529399971@qq.com>
2019-01-09 20:56:38 -0800

    0.1

---
+++ src/main/java/NeteaseCrawler.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class NeteaseCrawler implements Crawler {
+    /**
+     * Init
+     */
+    void init();
+
+    /**
+     * Get a un crawled page, mark it as crawled
+     */
+    WebPage getUncrawlPage();
+
+    /**
+     * Add new pages to crawl list
+     */
+    List<WebPage> addToCrawlList(List<WebPage> webPages);
+
+    /**
+     * Save song to known song list
+     */
+    Song saveSong(Song song);
+
+    /**
+     * Get all know songs
+     */
+    List<Song> getSongs();
+
+    /**
+     * Get a un crawled page, parse its html and mark new pages
+     */
+    void doRun();
+
+    /**
+     * Main function
+     */
+    default void run() {
+        init();
+        doRun();
+    }
+}
